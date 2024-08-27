332dc6d64ba63758a99e1ae51a487c72acb66ef5
Joel Masiaga <masiagajoel001@gmail.com>
2024-08-28 00:27:55 +0300

    Updated project with user registration and authentication features

--- KuzaNdotoCampaign/KuzaNdotoCampaign/settings.py
+++ KuzaNdotoCampaign/KuzaNdotoCampaign/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_TEMPLATE_PACK = 'bootsrap5'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
